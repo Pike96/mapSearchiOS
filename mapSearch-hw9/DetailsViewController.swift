51c277e923bf16cac737c6e5c3af1c1dec9db0e9
Pike96 <linyucheng256@live.com>
2018-04-22 03:05:59 -0700

    Tab bar

--- mapSearch-hw9/DetailsViewController.swift
+++ mapSearch-hw9/DetailsViewController.swift
@@ -13,21 +13,63 @@ class DetailsViewController: UITabBarController {
     var name = ""
     var placeId = ""
     
+    @IBOutlet weak var myTabBar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(name)
-        print(placeId)
+        //self.title = name
+        navigationItem.title = name
+        var url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/forward-arrow.png")
+        var data = try? Data(contentsOf: url!)
+        let shareButton = UIBarButtonItem.init(image: UIImage(data: data!), style: .done, target: self, action: #selector(DetailsViewController.share))
+        url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/favorite-empty.png")
+        data = try? Data(contentsOf: url!)
+        let favButton = UIBarButtonItem.init(image: UIImage(data: data!), style: .done, target: self, action: #selector(DetailsViewController.fav))
         
+        self.navigationItem.rightBarButtonItems = [favButton, shareButton]
+        setTabBarItems()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    @objc func share() {
+        print("share")
+    }
+    @objc func fav() {
+        print("fav")
+    }
+    
+    func setTabBarItems(){
+        
+        let myTabBarItem1 = (self.tabBar.items?[0])! as UITabBarItem
+        var url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/info.png")
+        var data = try? Data(contentsOf: url!)
+        myTabBarItem1.image = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        myTabBarItem1.selectedImage = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        
+        let myTabBarItem2 = (self.tabBar.items?[1])! as UITabBarItem
+        url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/picture.png")
+        data = try? Data(contentsOf: url!)
+        myTabBarItem2.image = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        myTabBarItem2.selectedImage = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        
+        let myTabBarItem3 = (self.tabBar.items?[2])! as UITabBarItem
+        url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/google-maps.png")
+        data = try? Data(contentsOf: url!)
+        myTabBarItem3.image = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        myTabBarItem3.selectedImage = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        
+        let myTabBarItem4 = (self.tabBar.items?[3])! as UITabBarItem
+        url = URL(string: "http://cs-server.usc.edu:45678/hw/hw9/images/ios/review.png")
+        data = try? Data(contentsOf: url!)
+        myTabBarItem4.image = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)
+        myTabBarItem4.selectedImage = UIImage(data: data!)?.withRenderingMode(.alwaysTemplate)        
+    }
+    
     /*
     // MARK: - Navigation
 
--- mapSearch-hw9/ResultsViewController.swift
+++ mapSearch-hw9/ResultsViewController.swift
@@ -210,10 +210,10 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // Segue to the second view controller
-        self.performSegue(withIdentifier: "showDetails", sender: self)
         let item = self.resultsArr[indexPath.row]
         self.selectedName = item.name!
         self.selectedPlaceId = item.placeId!
+        self.performSegue(withIdentifier: "showDetails", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -225,61 +225,6 @@ class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewD
         detailsViewController.name = self.selectedName
         detailsViewController.placeId = self.selectedPlaceId
     }
-    
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
