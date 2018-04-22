0d0d42e3d7ba526cf3f6eef8f1e4607c10db2cae
Pike96 <linyucheng256@live.com>
2018-04-22 01:13:53 -0700

    Pagination & Details Storyboard

--- mapSearch-hw9/ResultsViewController.swift
+++ mapSearch-hw9/ResultsViewController.swift
@@ -15,10 +15,11 @@ struct ResultsItem {
     let name: String?
     let icon: NSURL
     let vicinity: String?
+    let placeId: String?
     let fav: Bool?
 }
 
-class ResultsViewController: UIViewController, UITableViewDataSource {
+class ResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let DOMAIN = "http://mapsearchpike96.rajgs5wdu2.us-west-1.elasticbeanstalk.com"
     var kwd = ""
@@ -27,9 +28,16 @@ class ResultsViewController: UIViewController, UITableViewDataSource {
     var from = ""
     var lat = 0.0
     var lon = 0.0
+    var page = 1
+    var nextPageToken = ""
     var resultsArr = [ResultsItem]()
+    var resultsAll = [AnyObject]()
+    var selectedName = ""
+    var selectedPlaceId = ""
     @IBOutlet var noItemsView: UIView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,31 +51,14 @@ class ResultsViewController: UIViewController, UITableViewDataSource {
         SwiftSpinner.show("Searching...")
         searchHandler()
         
-        self.tableView.backgroundView = noItemsView
+        self.tableView.backgroundView = self.noItemsView
         self.tableView.separatorStyle = .none
     }
     
     func searchHandler() {
         if from == "My location" || from == "Your location" {
-            //            let serverTrustPolicies: [String: ServerTrustPolicy] = [
-            //                "ip-api.com": .disableEvaluation
-            //            ]
-            //
-            //            let sessionManager = SessionManager(
-            //                serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
-            //            )
-            //            sessionManager.request("http://ip-api.com/json")
-            //            Alamofire.request("http://ip-api.com/json", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON{ (response) in
-            //                print(response.result)
-            //
-            //            }
             Alamofire.request("http://ip-api.com/json").responseSwiftyJSON { response in
-                print("Request: \(String(describing: response.request))")   // original url request
-                print("Response: \(String(describing: response.response))") // http url response
-                print("Result: \(response.result)")                         // response serialization result
-                
                 if let json = response.result.value {
-                    print(json) // serialized json response
                     self.lat = json["lat"].double!
                     self.lon = json["lon"].double!
                     self.apiSearch(kwd: self.kwd, ctg: self.ctg, dis: self.dis, lat: self.lat, lon: self.lon)
@@ -76,12 +67,7 @@ class ResultsViewController: UIViewController, UITableViewDataSource {
         } else {
             let loc = from.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
             Alamofire.request("\(DOMAIN)/location?loc=\(loc)", encoding: URLEncoding.default).responseSwiftyJSON { response in
-                print("Request: \(String(describing: response.request))")   // original url request
-                print("Response: \(String(describing: response.response))") // http url response
-                print("Result: \(response.result)")                         // response serialization result
-                
-                if let json = response.result.value {
-                    print("JSON: \(json)") // serialized json response
+                    if let json = response.result.value {
                     self.lat = json["lat"].double!
                     self.lon = json["lon"].double!
                     self.apiSearch(kwd: self.kwd, ctg: self.ctg, dis: self.dis, lat: self.lat, lon: self.lon)
@@ -95,31 +81,93 @@ class ResultsViewController: UIViewController, UITableViewDataSource {
         let category = ctg.replacingOccurrences(of: " ", with: "+", options: .literal, range: nil)
         let distance = dis == "" ? "10" : dis;
         Alamofire.request("\(DOMAIN)/list?keyword=\(keyword)&category=\(category)&distance=\(distance)&lat=\(lat)&lon=\(lon)").responseSwiftyJSON { response in
-                print("Request: \(String(describing: response.request))")   // original url request
-                print("Result: \(response.result)")                         // response serialization result
-                
                 if let json = response.result.value {
-                    print(json) // serialized json response
                     if json["status"].string! == "OK" {
                         //self.info.text = "has"
+                        self.page = 1
+                        self.resultsAll = []
                         let tempArr = json["results"].arrayValue
                         for i in tempArr {
                             let name = i["name"].stringValue
                             let icon = i["icon"].stringValue
                             let vicinity = i["vicinity"].stringValue
+                            let placeId = i["place_id"].stringValue
                             
-                            let item = ResultsItem(name: name, icon: NSURL(string:icon)!, vicinity: vicinity, fav: false)
+                            let item = ResultsItem(name: name, icon: NSURL(string:icon)!, vicinity: vicinity, placeId: placeId, fav: false)
                             self.resultsArr.append(item)
                         }
+                        self.resultsAll.append(self.resultsArr as AnyObject)
                         self.tableView.separatorStyle = .singleLine
                         self.tableView.backgroundView = nil
-                        self.tableView.reloadData()
                     } else {
-                        //self.info.text = "No"
+                        self.tableView.backgroundView = self.noItemsView
+                        self.tableView.separatorStyle = .none
                     }
+                    if json["next_page_token"].stringValue != "" {
+                        self.nextButton.isEnabled = true
+                        self.nextPageToken = json["next_page_token"].stringValue
+                    }
+                    self.tableView.reloadData()
                     SwiftSpinner.hide()
                 }
         }
+    }
+    
+    @IBAction func next(_ sender: UIButton) {
+        SwiftSpinner.show("Searching...")
+        self.page = self.page + 1
+        if self.page != 1 {
+            self.prevButton.isEnabled = true
+        } else {
+            self.prevButton.isEnabled = false
+        }
+        if self.page >= self.resultsAll.count {
+            Alamofire.request("\(DOMAIN)/nextpage?pagetoken=\(self.nextPageToken)", encoding: URLEncoding.default).responseSwiftyJSON { response in
+                if let json = response.result.value {
+                    //print(json["next_page_token"].stringValue)
+                    if json["next_page_token"].stringValue != "" {
+                        self.nextButton.isEnabled = true
+                        self.nextPageToken = json["next_page_token"].stringValue
+                    } else {
+                        self.nextButton.isEnabled = false
+                    }
+                    self.resultsArr = []
+                    let tempArr = json["results"].arrayValue
+                    for i in tempArr {
+                        let name = i["name"].stringValue
+                        let icon = i["icon"].stringValue
+                        let vicinity = i["vicinity"].stringValue
+                        let placeId = i["place_id"].stringValue
+                        
+                        let item = ResultsItem(name: name, icon: NSURL(string:icon)!, vicinity: vicinity, placeId: placeId, fav: false)
+                        self.resultsArr.append(item)
+                    }
+                    self.resultsAll.append(self.resultsArr as AnyObject)
+                    self.tableView.reloadData()
+                    SwiftSpinner.hide()
+                }
+            }
+        } else {
+            self.resultsArr = self.resultsAll[self.page - 1] as! [ResultsItem]
+            self.tableView.reloadData()
+            SwiftSpinner.hide()
+        }
+    }
+    
+    @IBAction func prev(_ sender: UIButton) {
+        SwiftSpinner.show("Searching...")
+        self.page = self.page - 1
+        if self.page != 1 {
+            self.prevButton.isEnabled = true
+        } else {
+            self.prevButton.isEnabled = false
+        }
+        
+        self.nextButton.isEnabled = true
+        
+        self.resultsArr = self.resultsAll[self.page - 1] as! [ResultsItem]
+        self.tableView.reloadData()
+        SwiftSpinner.hide()
     }
     
     override func didReceiveMemoryWarning() {
@@ -157,6 +205,25 @@ class ResultsViewController: UIViewController, UITableViewDataSource {
 
         // add code to download the image from fruit.imageURL
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Segue to the second view controller
+        self.performSegue(withIdentifier: "showDetails", sender: self)
+        let item = self.resultsArr[indexPath.row]
+        self.selectedName = item.name!
+        self.selectedPlaceId = item.placeId!
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // get a reference to the second view controller
+        let detailsViewController = segue.destination as! DetailsViewController
+        
+        // set a variable in the second view controller with the String to pass
+        detailsViewController.name = self.selectedName
+        detailsViewController.placeId = self.selectedPlaceId
     }
     
     /*
